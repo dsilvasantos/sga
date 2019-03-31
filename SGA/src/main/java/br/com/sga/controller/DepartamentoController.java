067f37159a191ec07c6b89ee97f245011ae9a59c
Ricardo Santos <Ricardo Santos@192.168.1.7>
2019-03-31 20:09:36 -0300

    Alteração no controller de Departamento e na tela de cadastro do mesmo.

--- SGA/src/main/java/br/com/sga/controller/DepartamentoController.java
+++ SGA/src/main/java/br/com/sga/controller/DepartamentoController.java
@@ -6,14 +6,40 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.sga.monitoramento.DAO.DepartamentoDAO;
+import br.com.sga.monitoramento.model.Departamento;
+
 
 @ManagedBean(name = "departamento")
 @ViewScoped
 public class DepartamentoController {
+	
+	private Departamento departamento = new Departamento();
+	
+	public Departamento getDepartamento() {
+		return departamento;
+	}
 
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
 
 	public List<String> getDepartamentos() {
 		DepartamentoDAO departamentoDAO = new DepartamentoDAO();
 		return departamentoDAO.recuperar();
+	}
+	
+	public void salvar(Departamento departamento){
+		DepartamentoDAO dp = new DepartamentoDAO();
+		dp.persist(departamento);	
+	}
+	
+	public void remove(Departamento departamento){
+			DepartamentoDAO dp = new DepartamentoDAO();
+			dp.remove(departamento);	
+	}
+	
+	public  void alterar(Departamento departamento){
+		DepartamentoDAO dp = new DepartamentoDAO();
+		dp.merge(departamento);
 	}
 }
--- SGA/src/main/java/br/com/sga/controller/UsuarioController.java
+++ SGA/src/main/java/br/com/sga/controller/UsuarioController.java
@@ -1,12 +1,14 @@
 package br.com.sga.controller;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.sga.monitoramento.model.Usuario;
 import br.com.sga.services.ControladorMensagens;
 
-@ManagedBean
+@ManagedBean(name = "usuario")
 @ViewScoped
 public class UsuarioController {
 	
@@ -14,6 +16,12 @@ public class UsuarioController {
 	private Usuario user = new Usuario();
 	
 	
+	public Usuario getUsuario(){
+		if(this.user != null){
+			
+		}
+		return user;
+	}
 	
 	public Usuario getUser() {
 		return user;
@@ -29,7 +37,6 @@ public class UsuarioController {
 		 }
 		 	cm.addMsgErro("login.incorreto");
 		 return "login.xhtml";
-		 
 	 }
 	
 }
