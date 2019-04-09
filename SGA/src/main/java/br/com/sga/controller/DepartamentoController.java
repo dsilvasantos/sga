979f5fe34d4f22e810ec2d76bdd97af74f169c2e
ricardo <ricardofer543200@gmail.com>
2019-04-09 19:09:51 -0300

    Alterações na parte de Departamento classe/tela

--- SGA/src/main/java/br/com/sga/controller/DepartamentoController.java
+++ SGA/src/main/java/br/com/sga/controller/DepartamentoController.java
@@ -2,8 +2,10 @@ package br.com.sga.controller;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.sga.monitoramento.DAO.DepartamentoDAO;
 import br.com.sga.monitoramento.model.Departamento;
@@ -29,9 +31,19 @@ public class DepartamentoController {
 	}
 	
 	public void salvar(Departamento departamento){
+		FacesMessage face = null;
+		if(departamento == null){
 		DepartamentoDAO dp = new DepartamentoDAO();
 		dp.persist(departamento);	
-	}
+		face = new FacesMessage("Departamento cadastro com Sucesso !");
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, face);
+		}
+			new FacesMessage("Probelma ao cadastrar Departamento !");
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, face);
+	
+	}		
 	
 	public void remove(Departamento departamento){
 			DepartamentoDAO dp = new DepartamentoDAO();
