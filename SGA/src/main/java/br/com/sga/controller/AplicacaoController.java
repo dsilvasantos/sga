eb97c1e2fff34b4bc3ab0ab2290dd4af070ff9e5
Thaís Duarte <thaiss.duartee@gmail.com>
2019-04-27 13:03:36 -0300

    ajustes na tela de login caso de uso Aplicacao (CRUD) ajustes na tela de Aplicação

---
+++ SGA/src/main/java/br/com/sga/controller/AplicacaoController.java
@@ -0,0 +1,49 @@
+package br.com.sga.controller;
+
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.sga.monitoramento.DAO.AplicacaoDAO;
+import br.com.sga.monitoramento.model.Aplicacao;
+
+
+@ManagedBean(name = "aplicacao")
+@ViewScoped
+public class AplicacaoController {
+	
+	private Aplicacao aplicacao = new Aplicacao();
+	
+	public Aplicacao getAplicacao() {
+		return aplicacao;
+	}
+
+	public void setAplicacao(Aplicacao aplicacao) {
+		this.aplicacao = aplicacao;
+	}
+
+	public void salvar(Aplicacao aplicacao){
+		FacesMessage face = null;
+		if(aplicacao != null){
+		AplicacaoDAO ap = new AplicacaoDAO();
+		ap.persist(aplicacao);	
+		face = new FacesMessage("Aplicacao cadastrada com Sucesso !");
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, face);
+		}
+
+	}		
+	
+	public void remove(Aplicacao aplicacao){
+			AplicacaoDAO ap = new AplicacaoDAO();
+			ap.remove(aplicacao);	
+	}
+	
+	public  void alterar(Aplicacao aplicacao){
+		AplicacaoDAO ap = new AplicacaoDAO();
+		ap.merge(aplicacao);
+	}
+}
