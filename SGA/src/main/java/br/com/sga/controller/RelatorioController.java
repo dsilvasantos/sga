6db04d13ed633f94669a94f2eedb2e5a81ecb329
Diego Santos <Diego Santos@192.168.1.154>
2019-04-29 23:35:39 -0300

    Criação de tela para relatorio

--- SGA/src/main/java/br/com/sga/controller/RelatorioController.java
+++ SGA/src/main/java/br/com/sga/controller/RelatorioController.java
@@ -1,11 +1,35 @@
 package br.com.sga.controller;
 
+import java.util.Date;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 @ManagedBean(name = "relatorio")
 @ViewScoped
 public class RelatorioController {
+
+	private Date dataInicial;
+	
+	private Date dataFinal;
+
+	public Date getDataInicial() {
+		return dataInicial;
+	}
+
+	public void setDataInicial(Date dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+
+	public Date getDataFinal() {
+		return dataFinal;
+	}
+
+	public void setDataFinal(Date dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+	
+	
 
 
 	
